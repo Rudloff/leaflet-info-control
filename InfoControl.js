7baa13423d9586b7287f13ae7cc1e45c62b0f226
Pierre Rudloff <contact@rudloff.pro>
2017-05-28 00:41:44 +0200

    New getContent and setContent functions

--- InfoControl.js
+++ InfoControl.js
@@ -9,5 +9,11 @@ var InfoControl = L.Control.extend({
         var container = L.DomUtil.create('div', 'info-control leaflet-control-attribution');
         container.innerHTML = this.options.content;
         return container;
+    },
+    getContent: function () {
+        return this.getContainer().innerHTML;
+    },
+    setContent: function (html) {
+        this.getContainer().innerHTML = html;
     }
 });
